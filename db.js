82618dc3244cfbe412d5ccfd8a8bd552bb4cb2e2
siddhant-k08 <kulkarni.sid0809@gmail.com>
2026-03-22 15:03:06 +0530

    feat: implementation of .env done

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "subscription_db",
-  password: "password",
-  port: 5432,
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "subscription_db",
+  password: process.env.POSTGRES_PASSWORD || "password",
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
